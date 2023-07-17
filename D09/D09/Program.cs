10134aaf84712a9171917e4beced858d27d5c3b0
roland31x <roland31x@gmail.com>
2023-07-18 01:15:12 +0300

    day 9 done

---
+++ D09/D09/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace D09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = File.ReadAllText(@"..\..\..\input.txt");
+            int driver = 0;
+            int level = 0;
+            int sum = 0;
+            int garbage = 0;
+            while(driver < line.Length) 
+            {
+                if (line[driver] == '!')
+                {
+                    driver += 2;
+                    continue;
+                }
+                if (line[driver] == '{')
+                    level++;
+                if (line[driver] == '}')
+                {
+                    sum += level;
+                    level--;
+                }
+                if (line[driver] == '<')
+                {
+                    driver++;
+                    while (true)
+                    {
+                        if (line[driver] == '!')
+                        {
+                            driver += 2;
+                            continue;
+                        }
+                        garbage++;
+                        if (line[driver] == '>')
+                        {
+                            garbage--;
+                            break;
+                        }                           
+                        driver++;
+                    }
+                }
+                driver++;
+            }
+            Console.WriteLine("Part 1 solution:");
+            Console.WriteLine(sum);
+            Console.WriteLine("Part 2 solution:");
+            Console.WriteLine(garbage);
+        }
+    }
+}
