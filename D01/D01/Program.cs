75709cde1279689144476afbb62a257bfe7e3132
roland31x <roland31x@gmail.com>
2023-07-17 16:12:00 +0300

    day 1 done

---
+++ D01/D01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace D01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"..\..\..\input.txt");
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == input[(i + 1) % input.Length])
+                    sum1 += input[i] - '0';
+                if (input[i] == input[(i + input.Length / 2) % input.Length])
+                    sum2 += input[i] - '0';
+            }
+            Console.WriteLine("Part 1 solution:");
+            Console.WriteLine(sum1);
+            Console.WriteLine("Part 2 solution:");
+            Console.WriteLine(sum2);
+        }
+    }
+}
